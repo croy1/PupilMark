797fc1cf7d88edb1cbb29c25577caf91a0b089ce
croy1 <Guest@stk-cs01-ws10.westlothian.sch.uk>
2015-11-24 10:27:35 +0000

    sprint1.3

--- PUPIL.java
+++ PUPIL.java
@@ -1,9 +1,13 @@
 
 public class PUPIL
 {
-    
+    //list of properties
+    private String fName;
+    private String sName;
+    private float mark;
     
     public PUPIL()
     {
+        //constructor
     }
 }
