b3ac781beba5c9801279705b7d29bb78f711ae43
croy1 <Guest@stk-cs01-ws10.westlothian.sch.uk>
2015-11-25 12:02:24 +0000

    sprint1.5

--- PUPIL.java
+++ PUPIL.java
@@ -14,4 +14,29 @@ public class PUPIL
         sName = "";
         mark = 0.0f;
     }
-}
+    
+    public void readPupilsMark(String dataItems)
+    {
+        //unpack string of row data into fields 
+        String[] rowItems = dataItems.split(",");
+        
+        //store each data item as instance property
+        fName = rowItems[0];
+        sName = rowItems[1];
+        mark = Float.parseFloat(rowItems[2]);
+        
+        //produce name and mark of the highest mark
+    }
+        
+        public String writeDetails()
+        {
+            String pupilData ="";
+            pupilData = pupilData.concat(fName);
+            pupilData = pupilData.concat(",");
+            pupilData = pupilData.concat(sName);
+            pupilData = pupilData.concat(",");
+            pupilData = pupilData.concat(Float.toString(mark));
+            return pupilData;
+        }
+        
+    }
