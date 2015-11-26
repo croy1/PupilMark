ad8fc784f511a6137e175959583a618ee9c1fca4
croy1 <Guest@stk-cs01-ws10.westlothian.sch.uk>
2015-11-26 16:27:53 +0000

    sprint2.6

--- SCHOOL.java
+++ SCHOOL.java
@@ -15,14 +15,25 @@ public class SCHOOL
     }
 
     //top level algorithm
-    public void processPupils()
+    public void processPupils() throws IOException
     {
         setUpPupilList();
         countHighestMark();
     }
 
-    private void setUpPupilList()
+    private void setUpPupilList() throws IOException
     {
+        //First user message
+        System.out.println("PupilMark School: Pupil Mark update");
+        System.out.println("** Preparing to read data file.");
+        
+        //read file, fetch data as string array containing rows
+        String[] dataRows = markFile.readCSVtable();
+        //calculate the number of pupil rows, skip headings
+        noOfPupils = dataRows.length - 1;
+        
+        //update user with number of rows with pupil details
+        System.out.println("**" + noOfPupils + "rows read.\n\n");
         //placeholder
     }
 
