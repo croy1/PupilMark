e0a014ab801f13c8171dff857cef1c3879498362
Guest User <Guest@stk-cs01-ws10.westlothian.sch.uk>
2015-12-08 10:35:07 +0000

    sprint4.5

--- SCHOOL.java
+++ SCHOOL.java
@@ -28,16 +28,16 @@ public class SCHOOL
         //First user message
         System.out.println("PupilMark School: Pupil Mark update");
         System.out.println("** Preparing to read data file.");
-        
+
         //read file, fetch data as string array containing rows
         String[] dataRows = markFile.readCSVtable();
         //calculate the number of pupil rows
         noOfPupils = dataRows.length;
-        
+
         //update user with number of rows with pupil details
         System.out.println("**" + noOfPupils + "rows read.\n\n");
         //placeholder
-        
+
         //prepare array for pupils
         pupilList = new PUPIL [noOfPupils];
         //create pupil objects and copy data
@@ -54,37 +54,33 @@ public class SCHOOL
         System.out.println("A report of the pupils with the highest mark\n");
         int topmark = 0;
         //start the count
-        int count = 0;
+        //int count = 0; 
         //loop for each item ; pupil
         for (int i = 0; i < noOfPupils; i++)
         {
             //decide if current item; pupil matches highest mark
-            if ((pupilList[i].getMark() > topmark))
-            {
-                //add 1 to the count for highest mark
-                topmark = pupilList[i].getMark();
-                count = count +1;
-                //display the details of the member
-                pupilList[i].displayDetails();
-                
-                //use new line to seperate rows in csv file, after 1st line
-                if (count>1)
+            if (pupilList[i].getMark() > pupilList[topmark].getMark()){
                 {
-                    fileContent = fileContent.concat("\n");
+                    //choose position of first value
+                    topmark = pupilList[i].getMark();
+                    //display the details of the member
+                    pupilList[i].displayDetails();
+
+                    //join on next line of data for writing to file
+                    fileContent = 
+                    fileContent.concat(pupilList[i].writeDetails());
                 }
-                //join on next line of data for writing to file
-                fileContent = 
-                fileContent.concat(pupilList[i].writeDetails());
             }
+            //display the final count ; mark 
+            System.out.print("The name is;" + pupilList + ", top mark is;" + topmark);
+            //A blank line to seperate the report from others
+            pupilList[topmark].displayDetails();
+            System.out.println();
+
+            //send for writing to file as a string containing all data
+            System.out.print("** Preparing to write data file.");
+            resultFile.writeCSVtable(fileContent);
+            System.out.println("**File written and closed.");
         }
-        //display the final count ; mark 
-        System.out.print("\nTotal for highest mark is ;" + topmark);
-        //A blank line to seperate the report from others
-        System.out.println();
-        
-        //send for writing to file as a string containing all data
-        System.out.print("** Preparing to write data file.");
-        resultFile.writeCSVtable(fileContent);
-        System.out.println("**File written and closed.");
     }
 }
